87781c4c2c0c2284aef6fc1994d664a7e0a78e80
Vuong Hy Khang <89653821+kanglee079@users.noreply.github.com>
2024-03-19 13:58:01 +0700

    Delete lib/apps/const/key.dart

--- lib/apps/const/key.dart
+++
@@ -1,7 +0,0 @@
-class MyKey {
-  static const String TOKEN_USER = 'TOKEN_USER';
-  static const String INFO_USER = 'INFO_USER';
-
-  static const String OPENAI_API_KEY =
-      "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";
-}
